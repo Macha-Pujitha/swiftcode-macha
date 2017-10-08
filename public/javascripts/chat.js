2717911f4bd9363889866a4793d84b6e1f360103
Macha Pujitha <macha.pujitha@gmail.com>
2017-10-08 04:44:08 +0000

    UI

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,4 +1,26 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope, $sce) {
+    $scope.messages = [
+    {
+        'sender' : 'USER',
+        'text' : 'hi',
+        'time' : '09:01 AM'
+    },
+    {
+            'sender' : 'BOT',
+            'text' : 'How can i help you',
+            'time' : '09:01 AM'
+    },
+    {
+             'sender' : 'USER',
+             'text' : 'News about Trump',
+             'time' : '09:02 AM'
+    },
+    {
+            'sender' : 'BOT',
+            'text' : 'Not available at the moment',
+            'time' : '09:02 AM'
+    }
+    ]
 });
